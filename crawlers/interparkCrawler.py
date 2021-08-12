996678ae8c238d3f3d6b843a729e2eac8a64e4d8
limkaram <limkaram@korea.ac.kr>
2021-08-12 17:16:09 +0900

    podo 호텔 리뷰 크롤링 추가 및 explicit wait 로직 추가

--- crawlers/interparkCrawler.py
+++ crawlers/interparkCrawler.py
@@ -1,71 +1,76 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from crawlers.utils import get_datefmt_text, check_contain_datefmt_text
 import subprocess
+import yaml
+import os
 
 
 class Crawler:
-    def __init__(self):
+    def __init__(self, wait_time: int=10):
+        with open(os.path.join('config', 'config.yaml')) as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+        self.chrome_path = config['chrome_path']
         self.driver = None
-        self.reviews = None
-        self.scores = None
-        self.score_texts = None
-        self.dates = None
+        self.wait = None
+        self.wait_time = wait_time
 
-    def open(self, url: str, driver_path: str, headless: bool=False, wait_time: int=3):
-        subprocess.Popen(r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chrometemp"')  # 디버거 크롬 구동
+    def open(self, url: str, driver_path: str):
+        subprocess.Popen(r'{} --remote-debugging-port=9222 --user-data-dir="C:\chrometemp"'.format(self.chrome_path))
         option = Options()
         option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-
-        if headless:
-            option.add_argument('headless')
         self.driver = webdriver.Chrome(driver_path, options=option)
+        self.wait = WebDriverWait(self.driver, self.wait_time)
         self.driver.get(url)
-        self.driver.implicitly_wait(wait_time)
 
-    def get_info(self):
-        self.reviews = [i.text.rstrip(' [more]') for i in self.driver.find_elements_by_class_name('conWrap')]
+    def get_info(self, wait: bool):
+        if not wait:
+            self.wait = WebDriverWait(self.driver, 0)
+
+        reviews = [i.text.rstrip(' [more]') for i in self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'conWrap')))]
+
         try:
-            self.scores = [i.text for i in self.driver.find_elements_by_class_name('scoreNum')]
-            self.score_texts = [i.text for i in self.driver.find_elements_by_class_name('scoreTxt')]
+            scores = [i.text for i in self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'scoreNum')))]
+            score_texts = [i.text for i in self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'scoreTxt')))]
 
-            if len(self.scores) != len(self.reviews):
-                for _ in range(len(self.reviews) - len(self.scores)):
-                    self.scores.append(None)
-                    self.score_texts.append(None)
+            if len(scores) != len(reviews):
+                for _ in range(len(reviews) - len(scores)):
+                    scores.append('-')
+                    score_texts.append('-')
         except Exception:
-            self.scores = [None for _ in range(len(self.reviews))]
-            self.score_texts = [None for _ in range(len(self.reviews))]
+            scores = ['-' for _ in range(len(reviews))]
+            score_texts = ['-' for _ in range(len(reviews))]
 
         dates = []
-        for element in self.driver.find_elements_by_class_name('txt'):
+
+        for element in self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'txt'))):
             text = element.text
             if check_contain_datefmt_text(text):
                 dates.append(get_datefmt_text(text))
-        self.dates = dates
 
-        return {'date': self.dates, 'score': self.scores, 'score_category': self.score_texts, 'review': self.reviews}
+        return {'date': dates, 'score': scores, 'score_category': score_texts, 'review': reviews}
 
     def get_pages_list(self):
         pages = []
-        for text in self.driver.find_element_by_class_name('pageNumBox').text.split('\n'):
+        for text in self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'pageNumBox'))).text.split('\n'):
             if text.isnumeric():
                 pages.append(int(text))
 
         return pages
 
-    def click_page(self, page: int, wait_time: int=1):
-        self.driver.find_element_by_xpath(f'/html/body/section/div/section/div[2]/ul/li[{page}]').click()
-        self.driver.implicitly_wait(wait_time)
+    def click_page(self, page: int):
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, f'/html/body/section/div/section/div[2]/ul/li[{page}]'))).click()
 
-    def click_pagebar(self, point: str, wait_time: int=1):
+    def click_pagemovebar(self, point: str):
         if point == 'left':
-            self.driver.find_element_by_xpath('/html/body/section/div/section/div[2]/button[1]').click()
+            self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/section/div/section/div[2]/button[1]'))).click()
         elif point == 'right':
-            self.driver.find_element_by_xpath('/html/body/section/div/section/div[2]/button[2]').click()
+            self.wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/section/div/section/div[2]/button[2]'))).click()
         else:
-            raise Exception('Invalid pagebar')
-        self.driver.implicitly_wait(wait_time)
+            raise Exception('Invalid pagemovebar')
 
     def quit(self):
         self.driver.quit()
--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import os
 import yaml
 from crawlers import interparkCrawler
-from pprint import pprint
 import pandas as pd
+import time
+import datetime
+
+pd.set_option('display.max_columns', None)
+PROJECT_ROOT_PATH = os.getcwd()
 
 
 class Main:
     def __init__(self):
-        with open('config/config.yaml') as f:
+        with open(os.path.join(PROJECT_ROOT_PATH, 'config', 'config.yaml')) as f:
             self.config = yaml.load(f, Loader=yaml.FullLoader)
         self.driver_path = os.path.join(r'C:\Users\LIMKARAM\PycharmProjects\ReviewCollector\chromedriver.exe')
 
     def interpark_crawling(self):
-        interpark_annex_url = self.config['interpark']['annex']
-        result_df = pd.DataFrame()
+        for hotel, url in self.config['interpark'].items():
+            print(f'[{hotel}] crawling start!!!')
+            df_info = pd.DataFrame()
+            crawler = interparkCrawler.Crawler(wait_time=3)
+            crawler.open(url=url, driver_path=self.driver_path)
+            crawler.click_pagemovebar(point='left')
 
-        crawler = interparkCrawler.Crawler()
-        crawler.open(url=interpark_annex_url, driver_path=self.driver_path, headless=False)
-        crawler.click_pagebar(point='left')
-
-        for page in crawler.get_pages_list():
-            crawler.click_page(page, wait_time=1)
-            info = crawler.get_info()
-            pprint(info)
-            result_df = result_df.append(pd.DataFrame(info))
-        result_df.to_csv(os.path.join('outputs', 'interpark_20210811.csv'), index=False)
+            for page in crawler.get_pages_list():
+                if page > 1:
+                    crawler.click_page(page)
+                time.sleep(1)
+                info = crawler.get_info(wait=False)
+                print(f'[page {page}]')
+                print(info['date'], '\n', info['score'], '\n', info['score_category'], '\n', info['review'], '\n')
+                df_info = df_info.append(pd.DataFrame(info))
+            crawler.quit()
+            df_info = df_info.sort_values(by='date').reset_index(drop=True)
+            print(df_info.info(), '\n')
+            present_date = datetime.datetime.now().strftime('%Y%m%d')
+            df_info.to_csv(os.path.join('outputs', f'interpark_{hotel}_{present_date}.csv'), index=False)
 
 
 if __name__ == '__main__':
